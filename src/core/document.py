6d24b5bf03fd1bd37ce081ad0a3da38a4ceeb5c9
Ben Hur Bahia do Nascimento <benhurnascimento@gmail.com>
2016-07-12 20:56:38 -0300

    Document class updated for a more
    Use flow of document class changed to construct, configure and
    initialize

--- src/core/document.py
+++ src/core/document.py
@@ -2,35 +2,55 @@ from math import log
 
 class DocumentCollection(object):
 	"""docstring for DocumentCollection"""
+	tf_weighting_schemes = ('binary','raw frequency','log normalization',
+		'double normalization', 'double normalization k')
+
+	df_weighting_schemes = ()
 
 	def __init__(self, tf, df):
 		super(DocumentCollection, self).__init__()
-		self.documents = list()
-		self.tf_weighting_scheme = tf
-		self.df_weighting_scheme = df
+		self._documents = list()
+		self._tf_weighting_scheme = tf
+		self._df_weighting_scheme = df
 
 	def add_document(self, doc):
 		assert(type(doc) is Document)
 		assert(document.initialized())
+		self.documents
+
+	def build_from_filenames(self, filenames):
+		pass
+		# TODO implementar
 
 class Document(object):
-	"""docstring for Document"""
-	tf_weighting_schemes = ('binary','raw frequency','log normalization',
-		'double normalization', 'double normalization k')
+	"""
+	Class definition for a text document. You may inform
+	which term frequency weighting scheme should be used, the standard is
+	log normalization. 
 
-	def __init__(self, tf):
+	Possible weighting schemes: 'binary','raw frequency','log normalization',
+	'double normalization', 'double normalization k'
+	"""
+	tf_weighting_schemes = {'binary','raw frequency','log normalization',
+		'double normalization', 'double normalization k'}
+
+	def __init__(self, text=''):
 		super(Document, self).__init__()
 		self._words = None
 		self._frequencies = None
 		self._stopwords = None
-		self._init = False
+		self._text = text
+		self.tf_weighting_scheme = 'log normalization'
+		if text:
+			self._init = True
+		else:
+			self._init = False
 
-		assert(tf in Document.tf_weighting_schemes)
-		self.tf_weighting_scheme = tf
-
-
-	def initialized(self):
-		return self.init
+	def is_initialized(self):
+		"""
+		Return true if this object is initialized or false otherwise.
+		"""
+		return self._init
 
 	def _raw_frequency(self, words):
 		freq = dict()
@@ -89,6 +109,11 @@ class Document(object):
 		"""
 		# TODO implementar
 
+	def set_stop_words_file(self, filename, sep=' '):
+		stopwords = open(filename).read().split(sep)
+		self.set_stop_words(stopwords)
+
+
 	def set_stop_words(self, stopwords):
 		"""
 		Set the stop words for this document.
@@ -100,44 +125,57 @@ class Document(object):
 		for sw in stopwords:
 			assert(type(sw) is str)
 
-		self.stopwords = set(stopwords)
+		self._stopwords = set(stopwords)
 
 	def __is_not_stopword(self, word):
 		return word not in self.stopwords
 
-	def build_from_filename(self, filename):
+	def create_from_filename(self, filename):
 		"""
 		Build a document from a filename
 		"""
-		self.build_from_str(open(filename).read())
+		self.create_from_str(open(filename).read())
 
-	def build_from_str(self, text, readlines=False):
+	def create_from_str(self, text, readlines=False):
 		"""
 		Create a document from a string.
 		"""
 		assert(type(text) is str)
 		assert(len(text)>0)
+		self._text = text
+		self._init = False
 
-		self.init = True
+	def init(self):
+		"""
+		Initialize the document with the configuration given.
+		"""
+		# TODO usar dados do método
+		text = self._text
+		self._text = None
 
-		text = text.replace('\n','')
-		text = text.replace('.','')
-		text = text.replace(',','')
+		self._words = text.replace('\n','')
+		self._words = text.replace('.','')
+		self._words = text.replace(',','')
 
-		words = text.split(' ')
-		words = [w.lower() for w in words]
-		if self.stopwords:
-			words = list(filter(self.__is_not_stopword, words))
+		self._words = text.split(' ')
+		self._words = [w.lower() for w in words]
+		if self._stopwords:
+			self._words = list(filter(self.__is_not_stopword, words))
 		
-		self._frequencies = self._calculate_tf(words)
-		self._words = words
+		self._frequencies = self._calculate_tf(self._words)
+		self._init = True
+
 
 def main():
-	doctext = open('example_document.txt').read()
-	doc = Document('log normalization')
+	doc = Document()
+	doc.create_from_filename('example_document.txt')
 	doc.set_stop_words(['to','of','a','for','it','be','that','the'])
-	doc.build_from_str(doctext)
-	print(doc.get_term_frequency('tf-idf'))
+	doc.init()
+	
+
+	#doccol = DocumentCollection()
+	
+
 
 if __name__ == '__main__':
 	main()
